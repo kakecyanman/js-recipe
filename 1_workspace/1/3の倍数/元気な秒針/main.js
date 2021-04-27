3ffa65ff9db364f658a798db9c54b1d0fdbf9e22
kakecyanman <a.k.2903soccer@icloud.com>
2021-04-27 16:14:15 +0900

    元気な秒針

--- 1_workspace/1/3の倍数/元気な秒針/main.js
+++ 1_workspace/1/3の倍数/元気な秒針/main.js
@@ -1,25 +1,33 @@
+const display = document.getElementById("display")
 const button = document.getElementById("button")
+
 let count = 0
 
 const countUp = function() {
-  if (count > 100) {
-    count += 1
-    if (count % 3 === 0) {
-      console.log(`${count / 100}!!!!!!!`)
-    } else {
-      console.log(count / 100)
-    }
+  count += 1
+  if (count % 3 === 0) {
+    console.log(`${count}!!!!!!!`)
   } else {
-    console.log("終わり")
+    console.log(count)
   }
+  display.textContent = count
 }
+
+// const genkiFunction = function(count) {
+//   // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+//   if (count % 3 === 0) {
+//     console.log(`${count}!!!!!!!`)
+//   } else {
+//     console.log(count)
+//   }
+// }
 
 let id = null
 
 button.onclick = function() {
   if (id === null) {
     // start
-    id = setInterval(countUp, 10)
+    id = setInterval(countUp, 1000)
     button.textContent = "stop"
   } else {
     // stop
